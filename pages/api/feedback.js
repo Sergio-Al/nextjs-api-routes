9de900f357b9c5ae282cdc2430260ce2b904a7d4
Sergio-Al <sergio.alejandro.ml900@gmail.com>
2024-04-20 15:59:58 -0400

    Feat. using API routes to get data

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -1,6 +1,15 @@
 import fs from "fs";
 import path from "path";
 
+function buildFeedbackPath() {
+  return path.join(process.cwd(), "data", "feedback.json");
+}
+
+function extractFeedback(filePath) {
+  const fileData = fs.readFileSync(filePath);
+  return JSON.parse(fileData);
+}
+
 // Any code I wrote here will never end up in any client side bundle
 // It will never be exposed!
 function handler(req, res) {
@@ -15,14 +24,15 @@ function handler(req, res) {
     };
 
     // store that in a db or in a file...
-    const filePath = path.join(process.cwd(), "data", "feedback.json");
-    const fileData = fs.readFileSync(filePath);
-    const data = JSON.parse(fileData);
+    const filePath = buildFeedbackPath();
+    const data = extractFeedback(filePath);
     data.push(newFeedback);
     fs.writeFileSync(filePath, JSON.stringify(data));
     res.status(201).json({ message: "Success!", data: newFeedback });
   } else {
-    res.status(200).json({ message: "This works!" });
+    const filePath = buildFeedbackPath();
+    const data = extractFeedback(filePath);
+    res.status(200).json({ data: data });
   }
 }
 
