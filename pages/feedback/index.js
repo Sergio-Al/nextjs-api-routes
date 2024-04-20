ca54f73834e0713909adb96bb56e5018acb24924
Sergio-Al <sergio.alejandro.ml900@gmail.com>
2024-04-20 16:11:49 -0400

    Feat. Using API routes for Pre-Rendering pages

---
+++ pages/feedback/index.js
@@ -0,0 +1,25 @@
+import { buildFeedbackPath, extractFeedback } from "../api/feedback";
+
+function FeedbackPage(props) {
+  return (
+    <ul>
+      {props.feedbackItems.map((item) => (
+        <li key={item.id}>{item.text}</li>
+      ))}
+    </ul>
+  );
+}
+
+export async function getStaticProps() {
+  // You should not use fetch with your own api route
+  const filePath = buildFeedbackPath();
+  const data = extractFeedback(filePath);
+
+  return {
+    props: {
+      feedbackItems: data,
+    },
+  };
+}
+
+export default FeedbackPage;
